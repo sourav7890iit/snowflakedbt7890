864eee6f01fc906d13e3f8ea9f756d7df20c7147
Sourav Das <daslakshman1998@gmail.com>
2024-12-17 08:33:33 +0000

    new

--- models/customer_stging/stg_payment.sql
+++ models/customer_stging/stg_payment.sql
@@ -3,6 +3,8 @@ select * from {{ source('snowflake_raw_src', 'raw_payments') }}
 ),
 final as (
 select id as PAYMENT_ID,
-ORDER_ID,PAYMENT_METHOD,AMOUNT from a 
+ORDER_ID,
+PAYMENT_METHOD AS PAYMENT_MODE,
+AMOUNT/100 AS SALES_AMOUNT from a 
 )
 select * from final
